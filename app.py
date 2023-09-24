c0b1a546f63d595a9b50a3a3efa15aec7c5e6cdd
Nasi.C <nasiullha.chaudhari@secomind.ai>
2023-09-24 12:26:04 +0530

    Updated app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ def index():
     mem_metric = psutil.virtual_memory().percent
     Message = None
     if cpu_metric > 80 or mem_metric > 80:
-        Message = "High CPU or Memory Detected, scale up!!!"
+        Message = "High CPU or Memory Detected, Please scale up!!!"
     return render_template("index.html", cpu_metric=cpu_metric, mem_metric=mem_metric, message=Message)
 
 if __name__=='__main__':
